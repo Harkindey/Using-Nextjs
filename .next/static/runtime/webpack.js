18cdf37271f76af37f962df94cd22d4bc29d015a
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2019-08-30 23:42:56 +0100

    Passing Data via Query Strings

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "417fffc29e706058eeb6";
+/******/ 	var hotCurrentHash = "f84422d76e7541ea0ca6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
