ef99453bc48a9ad98be7eb7015a8a704aa1615ec
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2019-08-31 14:48:44 +0100

    Implement the Post Page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3064affffa19f215bcd3";
+/******/ 	var hotCurrentHash = "88b92c0b0a5e15fbd898";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
